26299454f048ddf164bdfcafbb2546562067f029
Jeremy Chen <jeremy886@gmail.com>
2018-02-02 07:27:32 +1100

    Update index.php
    quick test

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 echo "<h1>Check that out!</h1>";
 echo "<p>You've just created your first Web App for Containers app!</p>";
+echo "<p>And now you are modifying the welcome page in the app.</p>
 ?>
